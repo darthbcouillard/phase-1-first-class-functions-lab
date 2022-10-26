615142a5dd77543aad8a10c9072750adbe49f354
brent couillard <brentcouillard@yahoo.com>
2022-10-25 20:06:00 -0500

    commit tree c4cdbf4cd9e9f246b942a903c8df0c21da04b933

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2)
+}
+
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(multi){
+    return function (fare){
+        return fare * multi
+    }
+}
+
+const fareDoubler = createFareMultiplier(2) 
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = (drivers, driverFunction) => {
+    return driverFunction(drivers)
+    }
+
